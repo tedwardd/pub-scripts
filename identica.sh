78c2f215cafe33f897f74dfab80b647cc904dd1d
Ted W <ted.l.wood@gmail.com>
2011-06-23 18:30:30 -0400

    Version 1.1 change log * Improved documentation * re-wrote script mostly from ground up. Now significantly shorter * Added Usage section

--- identica.sh
+++ identica.sh
@@ -3,44 +3,41 @@
 # Author: Ted Wood (ted.l.wood@gmail.com)
 # Date: 2011-06-23
 #
-# There are two sections to this script. 
-# Section 1 will prompt you for your username, password and status seperately 
-# so that you don't have to leave your credintials hard coded to the script.
-# Section 2 will allow you to hard code your username and password so that
-# you can use the following syntax to post an update: "identica.sh foo status".
+# Take the input from $2, prompt for a password and then push $2 to identi.ca.
+# This is done using curl and identi.ca API. Adjust the information in the
+# section labeled "USER INFO" before using this script.
 
-## Section 1
+# ********************
+# **** USER INFO ***** 
+# ********************
+username="Username" #
 
-# You can hard code your username in the line below to save some typing.
-username="Your_Username"
+# *****************************************************************************
+# ******* You don't need to edit anything below here if you don't want. *******
+# *****************************************************************************
+status=$2
+post() {
+	if [ "$username" = "Username" ]
+		then
+		echo "You need to edit $0 and add your username to the "USER INFO" section before you can use the script."
+		exit 1
+	fi
+	# Prompt user for password and hide user input
+	stty -echo
+		while [ -z $pass ]
+			do
+		read -p "Enter your password: " pass
+		done
+	stty echo
 
-read -p "Enter identi.ca username [$username]: " username2
-if [ ! -z $username2 ]
-	then
-	username=$username2
-fi
-
-while [ -z $pass ]; do
-	read -p "Enter identi.ca password: " pass
-done
-
-while [ -z $status ]; do
-	read -p "Status update: " status
-done
-
-curl -u $username:$pass -d status="$status" https://identi.ca/api/statuses/update.xml
-
-## End Section 1
-
-## Section 2
-#
-# If you choose to use section 2 instead of section 1 please be careful.
-# I recommend setting the file permissions to 700 w/ "chmod 700 identica.sh"
-#
-################### 
-## User Settings (edit these to match your information)
-#username="Some_User"
-#pass="Some_pass"
-####################
-#
-#curl -u $username:$pass -d status="$1" https://identi.ca/api/statuses/update.xml
+	# Pass the information to curl
+	curl -u $username:$pass -d status="$status" https://identi.ca/api/statuses/update.xml
+}
+case "$1" in
+	post|update)
+		post
+		;;
+	*)
+		echo -e $"Usage: $0 {post|update} \"Text to post\"\n\tPost the text inside of the quotes."
+		RETVAL=1
+esac
