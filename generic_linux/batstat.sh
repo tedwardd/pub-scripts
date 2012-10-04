24b9ded6c214c0d0b3c6c0c1389e5d5904164a0e
Ted W <ted.l.wood@gmail.com>
2012-10-04 11:10:53 -0400

    Added a ChangeLog.txt and modified batstat.sh to work with apci and acpitool

--- generic_linux/batstat.sh
+++ generic_linux/batstat.sh
@@ -1,1 +1,19 @@
-acpitool -B | grep Remaining | awk '{print $6 " " $7}'
+## Author: Ted Wood
+## Email: ted.l.wood@gmail.com
+## Date: 2012-10-04
+###############################################################################
+## Since the acpitool project page has been down for some time, I've re-written
+## This utility to use the generic acpi utility. You can get this from
+## slackbuilds.org, if you are running slackware, or from your distributions'
+## prefered package management system.
+#
+## If you wish to use the acpitool method instead of the acpi method, uncomment
+## out the "ACPITOOL" section and comment out the "NO ACPITOOL" section.
+#
+###############################################################################
+
+### ACPITOOL
+#acpitool -B | grep Remaining | awk '{print $6 " " $7}'
+
+### NO ACPITOOL
+acpi -b | awk -F, '{print $2 $3}' | awk '{print $1" - "$2}'
