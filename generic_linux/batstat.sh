5634189d92e566f8f1a522aa1cebaf3ee3efd021
elw <ted.l.wood@gmail.com>
2013-07-10 13:05:59 -0400

    Added support for FreeBSD with acpiconf to batstat.sh

--- generic_linux/batstat.sh
+++ generic_linux/batstat.sh
@@ -12,21 +12,28 @@
 #
 ###############################################################################
 
+### ACPICONF
+# This will work on BSD systems that use the acpiconf utility instead of acpitool
+# You may have to change "0" to reflect the ID of your battery if not "0"
+percent=$(acpiconf -i 0 | grep "Remaining capacity:" | awk '{print $3}')
+time=$(acpiconf -i 0 | grep "Remaining time:" | awk '{print $3}')
+echo "${percent} | ${time}"
+#
 ### ACPITOOL
 #acpitool -B | grep Remaining | awk '{print $6 " " $7}'
-
+#
 ### NO ACPITOOL
 
-# Check if plugged in
-plug=$(acpi -a | awk '{print $3}')
+##Check if plugged in
+##plug=$(acpi -a | awk '{print $3}')
 
-# If not plugged in, show time remaining
-if [ $plug == "off-line" ]; then
-    acpi -b | awk -F, '{print $2 $3}' | awk '{print $1" - "$2}'
-# If plugged in, only show percentage
-elif [ $plug == "on-line" ]; then
-    acpi -b | awk '{print $4}'
-# This should never happen, but if it does, add a condition for it
-else
-    echo "NULL"
-fi
+## If not plugged in, show time remaining
+##if [ $plug == "off-line" ]; then
+##acpi -b | awk -F, '{print $2 $3}' | awk '{print $1" - "$2}'
+#If plugged in, only show percentage
+#elif [ $plug == "on-line" ]; then
+#acpi -b | awk '{print $4}'
+#This should never happen, but if it does, add a condition for it
+#else
+#echo "NULL"
+#fi
